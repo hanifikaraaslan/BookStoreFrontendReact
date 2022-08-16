d5fbff666f7ca1e7f21dcdf755da5b356ce88ccf
hanifikaraaslan <hanifi44karaaslan@gmail.com>
2022-08-16 22:27:45 +0300

    16-08-22 22.27

--- src/components/book/BookCard.js
+++ src/components/book/BookCard.js
@@ -6,14 +6,13 @@ import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -68,6 +67,10 @@ export default function BookCard({book}) {
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
+        <IconButton>
+        <AddShoppingCartIcon />
+        </IconButton>
+        
         <ExpandMore
           expand={expanded}
           onClick={handleExpandClick}
